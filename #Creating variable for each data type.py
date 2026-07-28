7274d3f542ddc94d8a7f7b5d2ef200727d7f53ee
mahakularunima990-max <mahakularunima990@gmail.com>
2026-07-28 11:02:28 +0530

    Add files via upload

---
+++ #Creating variable for each data type.py
@@ -0,0 +1,15 @@
+#Creating variable for each data type 
+Age=21
+print(type(Age))
+pi=3.14
+print(type(pi))
+name="Arunima"
+print(type(name))
+is_valid=True
+print(type(is_valid))
+list=[1,2,3,4,5]
+print(type(list))
+tuple=(1,2,3,4,5)
+print(type(tuple))
+student_details={"name":"Arunima"}
+print(type(student_details))
---
+++ #finding sum,difference,product,division.py
@@ -0,0 +1,11 @@
+#finding sum,difference,product,division 
+num1=12
+num2=2
+sum=num1+num2
+print(sum)
+difference=num1-num2
+print(difference)
+product=num1*num2
+print(product)
+divide=num1/num2
+print(divide)
