b644bdb0ce99d7b593e42491965c0e8edb2f2640
HP <hp@example.com>
2026-07-28 11:45:43 +0530

    Add DAY2 files

---
+++ # To store variables.py
@@ -0,0 +1,11 @@
+# To store variables 
+student_name="Arunima Mahakul"
+Age=21
+Course_name="Btech"
+Course_fee=400000
+Is_enrolled="cse"
+print(student_name)
+print(Age)
+print(Course_name)
+print(Course_fee)
+print(Is_enrolled)
---
+++ #converting to integer and add.py
@@ -0,0 +1,6 @@
+#converting to integer and add 
+str="125"
+num=int(str)
+print(num)
+add=num+50
+print(add)
